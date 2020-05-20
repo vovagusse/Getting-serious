f4663e7dddafa53f09cff0ed49f2139106f2e44e
Artyom <61916942+vovagusse@users.noreply.github.com>
2020-05-21 01:17:18 +0300

    broken code. dont even try to launch it
    bruh

---
+++ drawing_grid.py
@@ -0,0 +1,18 @@
+#Drawing grid 
+
+grid = []
+
+def set_grid_sise(grid):
+    input_num = int(input('Grid sise: '))
+    for i in range(input_num):
+        for i in range(input_num):
+            grid.append('-')
+
+    return(grid)
+
+set_grid_sise(grid)
+print(grid)
+for i in :
+    print('''{grid[i][i]}/n''') 
+
+#broken code might fix later
